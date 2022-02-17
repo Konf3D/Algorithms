e4f47038dde90853792ef7ac551cde41a0d3a826
Arthur Makushev <93376815+Konf3D@users.noreply.github.com>
2022-02-17 17:31:37 +0700

    Merge sort implementation
    Added merge sort

--- Sort.cpp
+++ Sort.cpp
@@ -51,4 +51,44 @@ void QuickSort(std::vector<int>& vector, int begin, int end)
         ++i, --j;
     }
 }
+void MergeSort(std::vector<int>& vector, std::vector<int>& buffer, int begin, int end)
+{
+    if (begin == end)
+        return;
+    int mid = (begin + end) / 2;
+    MergeSort(vector, buffer, begin, mid);
+    MergeSort(vector, buffer, mid + 1, end);
+
+    int left = begin;
+    int right = mid + 1;
+    int bufferIterator = left;
+    while (left <= mid && right <= end)
+    {
+        if (vector[left] <= vector[right])
+        {
+            buffer[bufferIterator] = vector[left];
+            ++left;
+        }
+        else
+        {
+            buffer[bufferIterator] = vector[right];
+            ++right;
+        }
+        ++bufferIterator;
+    }
+    for (int i = left; i <= mid; ++i)
+    {
+        buffer[bufferIterator] = vector[i];
+        ++bufferIterator;
+    }
+    for (int i = right; i <= end; ++i)
+    {
+        buffer[bufferIterator] = vector[i];
+        ++bufferIterator;
+    }
+    for (int i = begin; i <= end; ++i)
+    {
+        vector[i] = buffer[i];
+    }
+}
 }
